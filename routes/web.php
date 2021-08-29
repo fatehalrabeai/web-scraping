34e37a490da19e12d1a6c90d1b4e0ac0c4acb44c
Fateh Alrabeai <35519850+fatehalrabeai@users.noreply.github.com>
2021-08-29 20:25:35 +0300

    function uncmomn==mented

--- routes/web.php
+++ routes/web.php
@@ -18,67 +18,67 @@ Route::get('/home', function() {
     $deparments = Department::all();
    return view('welcome', compact('deparments'));
 });
+
+Route::get('/', function() {
+   $crawler = Goutte::request('GET',
+       'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaPgBiAEBmAExuAEZyAEM2AEB6AEB-AECiAIBqAIDuALM88qIBsACAdICJDA4ZDUxOTlhLWQ0ZWUtNGRkZi1iYWY1LTUzYWRkNTg1YzYyMNgCBeACAQ;sid=d26cb3e040516293245b39ee7fd73f42;dest_id=20117718;dest_type=city;highlighted_hotels=1065070&'
+   );
+//    $link = $crawler->filter('.bui-card__image')->link();
+
+   $images =  $crawler->filter('.sr_item_photo_link')->each(function ($node) {
+       return $node->filter('img')->attr('src');
+
+   });
+
+   $names = $crawler->filter('.sr-hotel__name')->each(function ($node)  {
+      return $node->text();
+   });
+
+   $ratings = $crawler->filter('.bui-review-score__badge')->each(function ($node)  {
+      return $node->text();
+   });
+
+
+   foreach ($images as $key => $image)
+   {
+       foreach ($names as $nameKey=>$name)
+
+       {
+           foreach ($ratings as $ratingKey=>$rating)
+           {
+               if ($key == $nameKey && $key == $ratingKey)
+               {
+                   Department::create([
+                       'name'=>$name,
+                       'image_url'=>$image,
+                       'rating'=>$rating,
+                   ]);
+               }
+           }
+       }
+
+   }
+
+
+
+   dd('done');
+//    $departments = Department::all();
 //
-//Route::get('/', function() {
-//    $crawler = Goutte::request('GET',
-//        'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaPgBiAEBmAExuAEZyAEM2AEB6AEB-AECiAIBqAIDuALM88qIBsACAdICJDA4ZDUxOTlhLWQ0ZWUtNGRkZi1iYWY1LTUzYWRkNTg1YzYyMNgCBeACAQ;sid=d26cb3e040516293245b39ee7fd73f42;dest_id=20117718;dest_type=city;highlighted_hotels=1065070&'
-//    );
-////    $link = $crawler->filter('.bui-card__image')->link();
-//
-//    $images =  $crawler->filter('.sr_item_photo_link')->each(function ($node) {
+//    foreach ($names as $name)
+//    {
+//        $departments->name = $name;
+//        $departments->save();
+//    }
+
+
+   dd('done');
+//   $images =  $crawler->filter('.sr_item_photo_link')->each(function ($node) {
 //        return $node->filter('img')->attr('src');
 //
 //    });
+//    dd($images);
+
 //
-//    $names = $crawler->filter('.sr-hotel__name')->each(function ($node)  {
-//       return $node->text();
-//    });
-//
-//    $ratings = $crawler->filter('.bui-review-score__badge')->each(function ($node)  {
-//       return $node->text();
-//    });
-//
-//
-//    foreach ($images as $key => $image)
-//    {
-//        foreach ($names as $nameKey=>$name)
-//
-//        {
-//            foreach ($ratings as $ratingKey=>$rating)
-//            {
-//                if ($key == $nameKey && $key == $ratingKey)
-//                {
-//                    Department::create([
-//                        'name'=>$name,
-//                        'image_url'=>$image,
-//                        'rating'=>$rating,
-//                    ]);
-//                }
-//            }
-//        }
-//
-//    }
-//
-//
-//
-//    dd('done');
-////    $departments = Department::all();
-////
-////    foreach ($names as $name)
-////    {
-////        $departments->name = $name;
-////        $departments->save();
-////    }
-//
-//
-//    dd('done');
-////   $images =  $crawler->filter('.sr_item_photo_link')->each(function ($node) {
-////        return $node->filter('img')->attr('src');
-////
-////    });
-////    dd($images);
-//
-////
-////    dd($link);
-////    return view('welcome');
-//});
+//    dd($link);
+//    return view('welcome');
+});
